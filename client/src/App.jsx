71855e8c0dff18b4f9cb8c77966fa9f368efc328
Hemant Raj <coder.hemantraj05@gmail.com>
2024-07-08 23:18:58 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        my app
+      </Router>
+    </div>
+  );
+};
+
+export default App;
